f5bda131f4b1feab08205f5fd1f1eecbc3c7b8d0
kaushiksahu18 <kaushiksahu_cse21@ggct.co.in>
2025-01-01 00:02:19 +0530

    update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SocketContextProvider>
-          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-        </SocketContextProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SocketContextProvider>{children}</SocketContextProvider>
+        </Suspense>
       </body>
     </html>
   );
